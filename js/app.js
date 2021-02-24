12e777af982d58f24cfd1fb982de331a90720a26
Ian Kiplagat <ianjkiplagat@gmail.com>
2021-02-24 23:34:53 +0300

    add form

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function feedback() {
+  var name = document.getElementById("name").value;
+  console.log(name);
+  if (name) {
+    alert(Hi + "name " + we have received your message. Thank you for reaching out to us)
+  }
+}
